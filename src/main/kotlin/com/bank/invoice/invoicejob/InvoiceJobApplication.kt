421abd0e7bb1019e85231d69408dc2296e935a72
Guilherme Torres <guilan.torres@gmail.com>
2025-12-23 20:02:06 -0300

    chore: init project

---
+++ src/main/kotlin/com/bank/invoice/invoicejob/InvoiceJobApplication.kt
@@ -0,0 +1,11 @@
+package com.bank.invoice.invoicejob
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InvoiceJobApplication
+
+fun main(args: Array<String>) {
+	runApplication<InvoiceJobApplication>(*args)
+}
---
+++ src/test/kotlin/com/bank/invoice/invoicejob/InvoiceJobApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bank.invoice.invoicejob
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InvoiceJobApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
