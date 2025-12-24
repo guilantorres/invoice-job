cf68b76bf4e9152915e6b0b65149759bd3827670
Guilherme Torres <guilan.torres@gmail.com>
2025-12-24 13:57:09 -0300

    feat: add invoice issuer

---
+++ src/main/kotlin/com/bank/invoice/job/application/InvoiceIssuerService.kt
@@ -0,0 +1,39 @@
+package com.bank.invoice.job.application
+
+import com.bank.invoice.job.domain.Invoice
+import com.bank.invoice.job.domain.provider.InvoiceProvider
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import kotlin.random.Random
+
+@Service
+class InvoiceIssuerService(
+    private val invoiceProvider: InvoiceProvider
+) {
+
+    private val logger = LoggerFactory.getLogger(InvoiceIssuerService::class.java)
+
+    fun issueInvoice() {
+        try {
+            val newInvoice = generateRandomInvoice()
+            val issuedInvoice = invoiceProvider.create(newInvoice)
+            logger.info("New invoice issued: ${issuedInvoice.id}")
+        } catch (e: Exception) {
+            logger.error("Error on issuing new invoice: ${e.message}")
+        }
+    }
+
+    // The following functions are for testing purposes
+    private fun generateRandomInvoice(): Invoice {
+        val amount = Random.nextLong(100, 10000)
+        val taxId = generateRandomCpf()
+        val name = "Spider Man \${Random.nextInt(100000)}"
+        return Invoice.createRequest(amount, taxId, name)
+    }
+
+    private fun generateRandomCpf(): String {
+        val n = { Random.nextInt(0, 9) }
+        return "${n()}${n()}${n()}.${n()}${n()}${n()}.${n()}${n()}${n()}-${n()}${n()}"
+    }
+
+}
