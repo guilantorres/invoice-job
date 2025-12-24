523fa73be835cec271bb864553b0dd1641aa1e62
Guilherme Torres <guilan.torres@gmail.com>
2025-12-24 16:12:15 -0300

    feat: create scheduler

--- src/main/kotlin/com/bank/invoice/job/InvoiceJobApplication.kt
+++ src/main/kotlin/com/bank/invoice/job/InvoiceJobApplication.kt
@@ -2,8 +2,10 @@ package com.bank.invoice.job
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class InvoiceJobApplication
 
 fun main(args: Array<String>) {
