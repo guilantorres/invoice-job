4c9ff4a10336617dbf32fd5fbd03839ec554a222
Guilherme Torres <guilan.torres@gmail.com>
2025-12-25 14:26:51 -0300

    feat: receiving invoice from sdk

--- src/main/kotlin/com/bank/invoice/job/api/StarkBankWebhookController.kt
+++ src/main/kotlin/com/bank/invoice/job/api/StarkBankWebhookController.kt
@@ -1,14 +1,15 @@
 package com.bank.invoice.job.api
 
-import com.starkbank.Event
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import tools.jackson.module.kotlin.jacksonObjectMapper
 
 @RestController
 @RequestMapping("/api/webhook/invoices")
 class StarkBankWebhookController() {
     private val logger = LoggerFactory.getLogger(StarkBankWebhookController::class.java)
+    private val mapper = jacksonObjectMapper()
 
     @PostMapping
     fun getEvent(
@@ -16,9 +17,27 @@ class StarkBankWebhookController() {
         @RequestHeader("Digital-Signature") signature: String
     ): ResponseEntity<String> {
         try {
-            logger.info("Received body: $body")
-            val event = Event.parse(body, signature)
-            // logger.info("Received event: $event")
+            val jsonBody = mapper.readTree(body)
+            val event = jsonBody.get("event")
+            val subscription = event.path("subscription")
+
+            if (subscription.stringValue() != "invoice") {
+                logger.warn("Subscription does not match subscription Invoice")
+                return ResponseEntity.badRequest().build()
+            }
+
+            val log = event.path("log")
+            val errors = event.path("errors")
+
+            if (errors.isArray && errors.size() > 0) {
+                logger.info("Event has errors: {}", errors.values())
+            }
+
+            val invoice = log.path("invoice")
+            val amount = invoice.path("amount").asLong()
+
+            logger.info("Amount received: {}", amount)
+
             return ResponseEntity.ok("Received")
         } catch (e: Exception) {
             logger.error(e.message)
