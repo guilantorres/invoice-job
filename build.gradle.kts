a0733d9ab8debb05eaa905b56c0df7624c83e3b0
Guilherme Torres <guilan.torres@gmail.com>
2025-12-26 14:04:53 -0300

    fix: add spring jpa for kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	kotlin("plugin.spring") version "2.2.21"
 	id("org.springframework.boot") version "4.0.1"
 	id("io.spring.dependency-management") version "1.1.7"
+	kotlin("plugin.jpa") version "1.9.22"
 }
 
 group = "com.bank.invoice"
